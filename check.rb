c0d8e139f97556761f5758aa677c8f95a42889b3
niktexnik <niktiek@yandex.ru>
2021-07-10 02:18:47 +0300

    xuyoviy result, toze  rezult

--- check.rb
+++ check.rb
@@ -7,28 +7,30 @@ class CheckUsername
   BASE_URL = 'https://api.github.com/'
 
   class << self
+
     def obr(freename)
       freename.each do |text|
         check_name(text)
+        print "Check name: #{text}, result: "
       end
     end
 
-    def check_name(text)
-      request('users/', text)
-    end
+def check_name(text)
+  request('users/', text)
+end
 
     def parse_response(response)
       return 'Something went wrong' if !response.code == 200
 
       body = JSON.parse(response.body)
       error = []
-      body.each do |_key, value|
+      body.each do |key, value|
         error << value
       end
       if error.include? 'Not Found'
-        print 'name avalible:'
+        print "- name avalible \n"
       else
-        print 'name not avalible'
+        print "- name not avalible \n"
       end
     end
 
